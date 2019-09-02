88856e1ee4b4b6fed1b56e5694e8673328114a17
REDPlays <40319851+REDPlays@users.noreply.github.com>
2019-09-02 09:51:00 -0400

    Part 1

---
+++ ClosestQuake/LargestQuakes.java
@@ -0,0 +1,59 @@
+
+/**
+ * Write a description of LargestQuakes here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+import java.util.*;
+
+public class LargestQuakes 
+{
+    public void findLargestQuakes() {
+            
+        EarthQuakeParser parser = new EarthQuakeParser();
+        String source = "data/nov20quakedata.atom";
+        //String source = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.atom";
+        
+        ArrayList<QuakeEntry> list  = parser.read(source);
+        System.out.println("read data for "+list.size());
+        
+        ArrayList<QuakeEntry> largest = getLargest(list, 50);
+        
+        for (QuakeEntry qe : largest) 
+        System.out.println(qe);
+                
+    }
+    
+    private ArrayList<QuakeEntry> getLargest(ArrayList<QuakeEntry> quakeData,
+            int howMany) {
+        
+        ArrayList<QuakeEntry> copy = new ArrayList<QuakeEntry>(quakeData);
+        ArrayList<QuakeEntry> largest = new ArrayList<QuakeEntry>();
+        
+        for (int i = 0; i<howMany; i++)
+            largest.add(copy.remove(indexOfLargest(copy)));
+        
+        return largest;
+    }
+    
+    /**
+     * This method returns an integer representing the index location in data 
+     * of the earthquake with the largest magnitude.
+     * @param data
+     * @return
+     */
+    private int indexOfLargest(ArrayList<QuakeEntry> data) {
+        
+        // Index of largest found quake so far
+        int idx = 0;
+        
+        for (QuakeEntry qe : data) {
+            double mag = qe.getMagnitude();
+            if (mag > data.get(idx).getMagnitude()) 
+                idx=data.indexOf(qe);   
+        }
+        
+        return idx;
+    }
+}
